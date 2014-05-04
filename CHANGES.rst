b84e0c3623ff5ed299071117328223136f1947fd
Baptiste Fontaine <batifon@yahoo.fr>
2014-05-04 23:33:17 +0200

    full doc

---
+++ CHANGES.rst
@@ -0,0 +1,6 @@
+v0.0.1
+------
+
+- initial release
+- support for ``add`` and ``remove`` operations
+- full online documentation
--- ctrie/__init__.py
+++ ctrie/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 """
+This module provides a compact trie (``CTrie``) implementation for Python.
+
 .. moduleauthor:: Baptiste Fontaine <b@ptistefontaine.fr>
 """
 
@@ -24,15 +26,58 @@ def _lcp(word1, word2):
     return word1 if len(word1) < len(word2) else word2
 
 
-# A compact trie
 class CTrie(object):
+    """
+    A compact trie. A trie, or prefix tree, is a data structure to efficiently
+    store a set of strings with a lot of shared prefixes, like URLs.
 
-    # Create a trie
+    Compact tries are recursively defined, a trie has zero or more other
+    child tries. A trie is said to be terminal if it contains the empty string.
+    Each child is labelled with a prefix for its own children. For example, one
+    compact trie that contains words "foo", "bar" and "foo42" has two children,
+    one labelled "foo" and the other labelled "bar". The last one is terminal
+    because once we read "bar" we can end our walk in the trie. The first one
+    is terminal for the same reason, but have one terminal child labelled "42".
+    In the following symbolic representation, a terminated child is represented
+    with a dot at the end of its label: ::
+
+         <empty>
+            |
+            +-- bar.
+            |
+            +-- foo.
+                 |
+                 +-- 42.
+
+    Each possible walk in the trie, starting from the root and ending on a
+    terminal node is a contained word. In the previous example, the empty
+    string is not present in the trie because the root node is not terminal.
+
+    A non-compact trie contains only one char per node.
+
+    >>> from ctrie import CTrie
+    >>> ct = CTrie()
+    >>> c.add("foo", "bar", "qux")
+    >>> "foo" in c
+    True
+    >>> "Bar" in c
+    False
+    >>> c.remove("foo")
+    >>> "foo" in c
+    False
+
+    Keyword arguments:
+            - ``terminal`` (``bool``, default: ``False``): specifies if the
+              trie contains the empty string. This is the same as:
+
+              >>> ct = CTrie()
+              >>> ct.add('')  # empty string
+    """
+
     def __init__(self, terminal=False):
         self._children = {}
         self.terminal = terminal
 
-    # Add one word to the trie
     def _add(self, word):
         # empty string
         if word == '':
@@ -74,10 +119,10 @@ class CTrie(object):
         self._children[word] = CTrie(terminal=True)
         return True
 
-    # Remove a word from the trie, return True or False depending on if the
-    # word was in the trie or not. This function might result in an unoptimized
-    # trie.
     def _remove(self, word):
+        """
+        Remove a word from the trie.
+        """
         if word == '':
             self.terminal = False
             return True
@@ -91,28 +136,35 @@ class CTrie(object):
 
         return False
 
-    # Add one or more words in the trie. The function returns True if all words
-    # were successfully added. It'll return False if one or more words were
-    # already present in the trie.
     def add(self, *words):
+        """
+        Add one or more words in the trie. The function returns ``True`` if all
+        words were successfully added. It'll return ``False`` if one or more
+        words were already present in the trie.
+        """
         ret = True
         for word in words:
             ret &= self._add(word)
 
         return ret
 
-    # Remove one or more words from the trie. The function returns True if all
-    # the words were successfully removed (i.e. they were in the trie before)
-    # or False if one or more words couldn't be found in the trie.
     def remove(self, *words):
+        """
+        Remove one or more words from the trie. The function returns ``True``
+        if all the words were successfully removed (i.e. they were in the trie
+        before) or ``False`` if one or more words couldn't be found in the
+        trie.
+        """
         ret = True
         for word in words:
             ret &= self._remove(word)
 
         return ret
 
-    # Check if the trie is empty
     def is_empty(self):
+        """
+        Return ``True`` if the trie is empty.
+        """
         return not (self._children or self.terminal)
 
     def __contains__(self, word):
