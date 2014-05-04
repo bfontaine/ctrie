a5ddd1ff38bde25473c4dab264cf92506fb75a17
Baptiste Fontaine <batifon@yahoo.fr>
2014-05-04 22:37:33 +0200

    #add fully implemented

--- ctrie/__init__.py
+++ ctrie/__init__.py
@@ -7,11 +7,22 @@
 __version__ = '0.0.1'
 
 
-def _isprefix(prefix, word):
+def _is_prefix(prefix, word):
     return word.startswith(prefix)
 
-def _sliceprefix(prefix, word):
+
+def _slice_prefix(prefix, word):
     return word[len(prefix):]
+
+
+# longuest common prefix
+def _lcp(word1, word2):
+    for i, (c1, c2) in enumerate(zip(word1, word2)):
+        if c1 != c2:
+            return word1[:i]
+
+    return word1 if len(word1) < len(word2) else word2
+
 
 class CTrie(object):
 
@@ -19,28 +30,47 @@ class CTrie(object):
         self._children = {}
         self.terminal = terminal
 
-
+    # Add one word to the trie
     def _add(self, word):
+        # empty string
         if word == '':
             if not self.terminal:
                 self.terminal = True
                 return True
             return False
 
-        # one children with a prefix
-        for prefix, child in self._children.items():
-            if _isprefix(prefix, word):
-                return child.add(_sliceprefix(prefix, word))
-
-        # no children
+        # no child
         if not self._children:
             self._children[word] = CTrie(terminal=True)
             return True
 
-        # TODO
+        # one child with a common prefix
+        for prefix, child in self._children.items():
+            # one child with the word as a prefix
+            if prefix == word:
+                if not child.terminal:
+                    child.terminal = True
+                    return True
+                return False
 
-        raise NotImplementedError
+            if _is_prefix(prefix, word):
+                return child.add(_slice_prefix(prefix, word))
 
+        # split a child prefix
+        for prefix, child in self._children.items():
+            lcp = _lcp(prefix, word)
+            if lcp:
+                middle = CTrie()
+                origin = _slice_prefix(lcp, prefix)
+                new_branch = _slice_prefix(lcp, word)
+                middle._children[origin] = child
+                middle._children[new_branch] = CTrie(terminal=True)
+                del self._children[prefix]
+                self._children[lcp] = middle
+                return True
+
+        self._children[word] = CTrie(terminal=True)
+        return True
 
     # Remove a word from the trie, return True or False depending on if the
     # word was in the trie or not.
@@ -50,14 +80,13 @@ class CTrie(object):
             return True
 
         for prefix, child in self._children.items():
-            if _isprefix(prefix, word):
-                ret = child._remove(_sliceprefix(prefix, word))
+            if _is_prefix(prefix, word):
+                ret = child._remove(_slice_prefix(prefix, word))
                 if child.is_empty():
                     del self._children[prefix]
                 return ret
 
         return False
-
 
     # Add one or more words in the trie. The function returns True if all words
     # were successfully added. It'll return False if one or more words were
@@ -69,7 +98,6 @@ class CTrie(object):
 
         return ret
 
-
     # Remove one or more words from the trie. The function returns True if all
     # the words were successfully removed (i.e. they were in the trie before)
     # or False if one or more words couldn't be found in the trie.
@@ -80,18 +108,17 @@ class CTrie(object):
 
         return ret
 
-
     # Check if the trie is empty
     def is_empty(self):
         return not (self._children or self.terminal)
-
 
     def __contains__(self, word):
         if word == '' and self.terminal:
             return True
 
         for prefix, child in self._children.items():
-            if _isprefix(prefix, word) and _sliceprefix(prefix, word) in child:
+            if _is_prefix(prefix, word) and \
+                    _slice_prefix(prefix, word) in child:
                 return True
 
         return False
